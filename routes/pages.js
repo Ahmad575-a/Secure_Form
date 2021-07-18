ab4ab3b7ba304cd12f22438730f4cd4bf0ab56ee
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-18 21:02:06 +0200

    render the pages and add profile

--- routes/pages.js
+++ routes/pages.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const authController = require('../Controllers/auth');
 
 router.get('/', (req, res) => {
-    res.render('Home')
+    res.render('Home', {
+        user: req.user
+    })
 })
+
 router.get('/register', (req, res) => {
     res.render('Register')
 })
+
+router.get('/login', (req, res) => {
+    res.render('Login')
+})
+
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+    if (req.user) {
+        res.render('Profile', {
+            user: req.user
+        });
+    } else {
+        res.redirect("/login");
+    }
+
+});
 
 module.exports = router
