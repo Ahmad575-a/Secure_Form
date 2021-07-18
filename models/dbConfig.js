9479f62f9576c1307f09bb01396d1faaab1b725d
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-18 21:08:30 +0200

    setup mysql

---
+++ models/dbConfig.js
@@ -0,0 +1,7 @@
+const mysql = require('mysql')
+exports.dbConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE
+})
