c2824e81b4757541c4e1b8ac9605cb245694bf01
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-07-20 23:34:30 +0200

    limited the requests for the login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../Controllers/auth')
+const limitter = require('express-rate-limit')
+const loginLimitter =limitter({
+    windowMs: 60 * 1000 ,
+    max: 100,
+    message:{
+        code:429,
+        message: 'Too many request my friend !! try later'
+    }
+})
+
 
 router.post('/register', authController.register)
-router.post('/login', authController.login)
+router.post('/login', loginLimitter, authController.login)
 router.get('/logout', authController.logout)
 
 module.exports = router
